d5626e1457a296f158c73106404c50a63590e85e
jabedalamakash <jabedalamakash00@gmail.com>
2023-08-14 12:51:58 +0600

    done

--- app/components/Nav.js
+++ app/components/Nav.js
@@ -5,27 +5,27 @@ import React from 'react';
 const Nav = () => {
     return (
       <main>
-            <div className='w-full sm:w-[80%] px-0 my-20 mx-auto'>
+            <div className='w-full sm:w-[80%] px-0 mt-20 mb-40 sm:20 mx-auto'>
 
                   <div className="navbar w-full sm:max-w-screen-xl mx-auto " >
-                    <div className="navbar-start">
+                    <div className="navbar-start ">
                       <div className="dropdown">
-                        <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                        <label tabIndex={0} className="btn mb-5 btn-ghost lg:hidden">
                           <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                         </label>
                         <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-0 text-xl rounded-box w-52">
-                          <li className='text-xl active:text-[#B68C5A]'>
+                          <li className='text-xl font-bold  hover:text-[#B68C5A]'>
                             <Link href="/">Home </Link></li>
-                          <li className='text-xl active:text-[#B68C5A] hover:text-[#B68C5A]'>
+                          <li className='text-xl font-bold  hover:text-[#B68C5A]'>
                             <Link href="/services">Services</Link>
                             </li>
-                          <li className='text-xl active:text-[#B68C5A] hover:text-[#B68C5A]'>
+                          <li className='text-xl font-bold  hover:text-[#B68C5A]'>
                             <Link href="/cases">Cases </Link>
                             </li>
-                          <li className='text-xl active:text-[#B68C5A] hover:text-[#B68C5A]'>
+                          <li className='text-xl font-bold hover:text-[#B68C5A]'>
                             <Link href="/blog">Blog</Link>
                           </li>
-                          <li className='text-xl active:text-[#B68C5A] hover:text-[#B68C5A]'>
+                          <li className='text-xl font-bold hover:text-[#B68C5A]'>
                             <Link href="/contactus">Contact Us </Link>
                             
                             </li>
